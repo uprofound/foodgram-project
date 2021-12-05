c58dbc7c4cd494c191e63685bd10990e39f670d4
uprofound <uzunitem@yandex.ru>
2021-12-05 20:55:57 +0300

    Added command to load ingredients data from csv-file to DB

---
+++ backend/recipes/management/commands/load_data.py
@@ -0,0 +1,17 @@
+import csv
+import os
+
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    help = 'Load ingredients data from csv-file to DB.'
+
+    def handle(self, *args, **options):
+        with open(os.path.join('recipes', 'data', 'ingredients.csv'), encoding='utf-8') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                name, measurement_unit = row
+                Ingredient.objects.get_or_create(name=name, measurement_unit=measurement_unit)
