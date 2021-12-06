4e50751c432047b5a878b8c278bbb3b62e38ffa0
uprofound <uzunitem@yandex.ru>
2021-12-06 15:35:51 +0300

    Added tests for ingredients endpoint

---
+++ tests/test_01_ingredients.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+class Test01IngredientAPI:
+    BASE_URL = '/api/ingredients/'
+
+    @pytest.mark.django_db(transaction=True)
+    def test_01_ingredients_not_auth(self, client):
+        response = client.get(self.BASE_URL)
+        assert response.status_code != 404, (
+            f'Страница `{self.BASE_URL}` не найдена, проверьте этот адрес в urls.py'
+        )
+        assert response.status_code == 200, (
+            f'Проверьте, что при GET запросе к `{self.BASE_URL}` без токена авторизации возвращается статус 200'
+        )
+
+    @pytest.mark.django_db(transaction=True)
+    def test_02_ingredient_post_not_allowed(self, client):
+        data = {}
+        response = client.post(self.BASE_URL, data=data)
+        assert response.status_code == 405, (
+            f'Проверьте, что создание ингредиента недоступно через POST запрос к `{self.BASE_URL}`'
+        )
