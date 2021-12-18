22c75a8767c753e65c91adac5ed92e0ad498d90b
uprofound <uzunitem@yandex.ru>
2021-12-19 01:25:48 +0300

    Added .env loading

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,16 +1,20 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1#%=-iokg*#3qn=$3p%i#^+fx=pvkjt8gtjwe)pjdgh#7kt@2('
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='*').split(', ')
 
 
 # Application definition
